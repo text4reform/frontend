2bc64c21640833a4811cc4a2fedb9290c887259c
judrov <judrov@gmail.com>
2014-08-15 01:48:50 -0500

    clean js into 2 files and add validations for invite a friend

---
+++ js/effects.js
@@ -0,0 +1,106 @@
+var $name    = $('#input_name');
+
+// Set up fancybox
+$(document).ready(function() {
+
+  $('.fancybox').fancybox();
+
+  $(".video").fancybox({
+    maxWidth : 853,
+    maxHeight : 455,
+    openEffect  : 'none',
+    closeEffect : 'none',
+    helpers : {
+      media : {}
+    }
+  });
+});
+
+// Scroll to anchors
+$('a[href*=#]').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var $target = $(this.hash);
+    $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+    if ($target.length) {
+      var targetOffset = $target.offset().top;
+      $('html,body')
+      .animate({scrollTop: targetOffset}, 700);
+
+      // Add focus name input box
+      if(targetOffset == 0){
+        $name.focus(); 
+      }
+      return false;
+    }
+  }
+});
+
+// When scroll over the invite panel, make the input
+// box for name or friend's phone number focused. 
+$(window).scroll(function() {
+  var yourName    = $('#input_yourname');
+  var friendPhone = $('#input_friendphone');
+  var inviteBtn   = $('#invite');
+  var invitePanel = $('#invite-panel');
+  var hT          = invitePanel.offset().top,
+  hH          = invitePanel.outerHeight(),
+  wH          = $(window).height(),
+  wS          = $(this).scrollTop();
+  
+  var isFocus = $(yourName).is(":focus") || $(friendPhone).is(":focus");
+  var isPosition = wS > (hT+hH-wH) && wS < (hT+hH-wH+400);
+
+  if(isPosition){
+    if(!isFocus){
+      if(!yourName.val().length){
+        $(yourName).focus();
+      }else if(!friendPhone.val().length){
+        $(friendPhone).focus();
+      }else{
+        $(inviteBtn).focus();
+      }
+    }
+  }else{
+    if(isFocus){
+      $(yourName).blur();
+      $(friendPhone).blur();
+      $(inviteBtn).blur();
+    }
+  }
+});
+
+// Fade in and fade out secondary navigation
+jQuery(function($){
+  var starting_position = $(window).height();
+  var navFixed          = $("#nav-fixed");
+  var minWidth          = 480;
+  
+  $(window).scroll(function() {
+    var yPos = ($(window).scrollTop());
+    // show sticky menu after these many (starting_position) pixels have been 
+    // scrolled down from the top and only when viewport width is greater than 480px.
+    if(yPos > starting_position && window.innerWidth > minWidth){ 
+      navFixed.fadeIn();
+    }else{
+      navFixed.fadeOut();
+    }
+  });
+});
+
+// Scroll to the top and focus name input box
+$('#signup').click(function(){
+  $('html,body').animate({scrollTop: 0}, 800,
+    function(){
+      $name.focus();
+    });
+  return false; 
+});
+
+// automatically populate name input on invite a friend panel
+// $name.change(function(){
+//   var input = $name.val(); 
+//   $('#input_yourname').val(input);
+// });
+
+
--- js/main.js
+++ js/main.js
@@ -3,57 +3,24 @@ var server        = '162.209.126.106:1234';
 // regular expression to validate phone and zip
 var phoneRegexp   = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 var zipCodeRegexp = /^([0-9]{5})(?:-[0-9]{4})?$/;
+var nameRegexp    = /^[A-Za-z ]+$/;
 
+// signup 
 var $name    = $('#input_name');
 var $phone   = $('#input_phone');
 var $zipcode = $('#input_zip');
-var $alert   = $('#message-alert'); 
+var $alert   = $('#message-alert');
 
+// invite a friend
+var $yourName    = $('#input_yourname');
+var $friendPhone = $('#input_friendphone'); 
+var $inviteAlert = $('#invite-message-alert');
+
+// Change warning message
 function warning(msg) {
   console.log("Warning");
   $alert.text(msg);
 }
-
-$(document).ready(function() {
-
-  // Set jumbotron's height to window height
-  // var windowHeight    = $(window).height();
-  // var windowWidth     = $(window).width();
-  // var jumbotronHeight = $('.jumbotron').height();
-  // var minWidth        = 0;
-
-  // if(windowHeight > jumbotronHeight && windowWidth > minWidth){
-  //   $('.jumbotron').height(windowHeight);
-  // }
-
-  $('.fancybox').fancybox();
-
-  $(".video").fancybox({
-    maxWidth : 853,
-    maxHeight : 455,
-    openEffect  : 'none',
-    closeEffect : 'none',
-    helpers : {
-      media : {}
-    }
-  });
-});
-
-jQuery(function($){
-  var starting_position = $(window).height();
-  var minWidth          = 480;
-  
-  $(window).scroll(function() {
-    var yPos = ($(window).scrollTop());
-    // show sticky menu after these many (starting_position) pixels have been 
-    // scrolled down from the top and only when viewport width is greater than 500px.
-    if(yPos > starting_position && window.innerWidth > minWidth){ 
-      $("#nav-fixed").fadeIn();
-    }else{
-      $("#nav-fixed").fadeOut();
-    }
-  });
-});
 
 // kill form submit
 $('form').submit(function(e) {
@@ -131,91 +98,79 @@ $('#submit').click(function() {
   return false; 
 });
 
-// Scroll to anchors
-$('a[href*=#]').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var $target = $(this.hash);
-    $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+function showAlert(msg){
+  $inviteAlert.text(msg);
+  $inviteAlert.fadeIn();
+  setTimeout(function(){
+    $inviteAlert.fadeOut();
+  }, 5000);
+}
 
-    if ($target.length) {
-      var targetOffset = $target.offset().top;
-      $('html,body')
-      .animate({scrollTop: targetOffset}, 700);
+// Invite a friend
+$('#invite').click(function() {
+  var btn     = $(this);
+  var name    = $yourName.val();
+  var phone   = $friendPhone.val();
 
-      if(targetOffset == 0){
-        $name.focus(); 
-      }
-      return false;
-    }
+  var invalidName  = "Whoops, invalid name.";
+  var invalidPhone = "Whoops, invalid phone number.";
+
+  // console.log("invite a friend");
+
+  // validate name
+  var result = nameRegexp.exec(name);
+  if(!result){
+    $yourName.val('').focus();
+    showAlert(invalidName);
+    return false;
+  }else{
+    name = result[0];
   }
-});
 
-// Scroll to the top
-$('#signup').click(function(){
-  $('html,body').animate({scrollTop: 0}, 800,
-    function(){
-      $name.focus();
-    });
+  // validate phone
+  result = phoneRegexp.exec(phone);
+  if(!result){
+    $friendPhone.val('').focus();
+    showAlert(invalidPhone);
+    return false;
+  }else{
+    phone = result[1] + result[2] + result[3];
+  }
+
+  showAlert("This feature is under development. Please come back next week.");
+  
+  // change button text to sending...
+  // btn.button('loading');
+
+  // submit name and phone to twilio
+  // $.ajax({
+  //   type: "post",
+  //   dataType: "json",
+  //   url: 'http://' + server + '/textme',
+  //   data: { zip: zipcode, number: phone },
+  //   crossDomain: true,
+  //   timeout: 2000,
+  //   success: function(data) {
+  //     console.log("success: ", data);
+  //     btn.button('reset');
+  //     if (data.exists) {
+  //       warning('We have already texted that number recently.');
+  //       $phone.val('');
+  //       $zipcode.val('');
+  //     } else {
+  //       // clear phone and zip input
+  //       warning('Thanks! We will contact you shortly via text.');
+  //       $phone.val('');
+  //       $zipcode.val('');
+  //     }
+  //   },
+  //   error: function(data) {
+  //     console.log("Error: ", data);
+  //     btn.button('reset')
+  //     // clear phone and zip input
+  //     $phone.val('');
+  //     $zipcode.val('');
+  //     warning('Sorry, we were unable to submit your information this time.');
+  //   }});
   return false; 
 });
-
-// change name input on invite a friend panel
-// $name.change(function(){
-//   var input = $name.val(); 
-//   $('#input_yourname').val(input);
-// });
-
-// when scroll over the invite panel, make the input
-// box for name or friend's phone number focused. 
-$(window).scroll(function() {
-  var yourName    = $('#input_yourname');
-  var friendPhone = $('#input_friendphone');
-  var hT          = $('#invite-panel').offset().top,
-      hH          = $('#invite-panel').outerHeight(),
-      wH          = $(window).height(),
-      wS          = $(this).scrollTop();
-  
-  if (wS > (hT+hH-wH) && wS < (hT+hH-wH+400)){
-    if($(yourName).is(":focus") || $(friendPhone).is(":focus")){
-      return;
-    }
-    else if(yourName.val().length == 0){
-      $(yourName).focus();
-    }else{
-      $(friendPhone).focus();
-    }
-  }else{
-    if($(yourName).is(":focus") || $(friendPhone).is(":focus")){
-      $(yourName).blur();
-      $(friendPhone).blur();
-    }
-  }
-
-});
-
-// change icon color when hover over. 
-// TODO: Can improve this to delete copy/paste.
-$('.twitter-button').hover(
-  function () {
-    $('.twitter-img').css({'-webkit-filter':'invert(100%)'});
-  }, 
-  function () {
-    $('.twitter-img').css({'-webkit-filter':'invert(50%)'});
-  });
-
-$('.fb-button').hover(
-  function () {
-    $('.fb-img').css({'-webkit-filter':'invert(100%)'});
-  }, 
-  function () {
-    $('.fb-img').css({'-webkit-filter':'invert(50%)'});
-  });
-
-$('.google-button').hover(
-  function () {
-    $('.google-img').css({'-webkit-filter':'invert(100%)'});
-  }, 
-  function () {
-    $('.google-img').css({'-webkit-filter':'invert(50%)'});
-  });
-
